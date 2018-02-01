0dabcfa46d0f0ae44cd5f6a97f8b5158cda1faa1
yangmeeso <yangmeeso@gmail.com>
2018-01-31 19:15:01 -0800

    exchange labels for grower and product

--- public/assets/js/highflow_jquery.js
+++ public/assets/js/highflow_jquery.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
     /* global moment */
   
-    // blogContainer holds all of our posts
+    // blogContainer holds all of our products
     var blogContainer = $(".blog-container");
     var postCategorySelect = $("#category");
     // Click events for the edit and delete buttons
     $(document).on("click", "button.delete", handlePostDelete);
     $(document).on("click", "button.edit", handlePostEdit);
-    // Variable to hold our posts
-    var posts;
+    // Variable to hold our products
+    var products;
   
-    // The code below handles the case where we want to get blog posts for a specific author
+    // The code below handles the case where we want to get blog products for a specific grower
     // Looks for a query param in the url for author_id
     var url = window.location.search;
     var authorId;
-    if (url.indexOf("?author_id=") !== -1) {
+    if (url.indexOf("?grower_id=") !== -1) {
       authorId = url.split("=")[1];
       getPosts(authorId);
     }
-    // If there's no authorId we just get all posts as usual
+    // If there's no authorId we just get all products as usual
     else {
       getPosts();
     }
   
   
-    // This function grabs posts from the database and updates the view
-    function getPosts(author) {
-      authorId = author || "";
+    // This function grabs products from the database and updates the view
+    function getPosts(grower) {
+      authorId = grower || "";
       if (authorId) {
-        authorId = "/?author_id=" + authorId;
+        authorId = "/?grower_id=" + authorId;
       }
-      $.get("/api/posts" + authorId, function(data) {
+      $.get("/api/products" + authorId, function(data) {
         console.log("Posts", data);
-        posts = data;
-        if (!posts || !posts.length) {
-          displayEmpty(author);
+        products = data;
+        if (!products || !products.length) {
+          displayEmpty(grower);
         }
         else {
           initializeRows();
@@ -42,30 +42,30 @@ $(document).ready(function() {
       });
     }
   
-    // This function does an API call to delete posts
+    // This function does an API call to delete products
     function deletePost(id) {
       $.ajax({
         method: "DELETE",
-        url: "/api/posts/" + id
+        url: "/api/products/" + id
       })
       .then(function() {
         getPosts(postCategorySelect.val());
       });
     }
   
-    // InitializeRows handles appending all of our constructed post HTML inside blogContainer
+    // InitializeRows handles appending all of our constructed product HTML inside blogContainer
     function initializeRows() {
       blogContainer.empty();
       var postsToAdd = [];
-      for (var i = 0; i < posts.length; i++) {
-        postsToAdd.push(createNewRow(posts[i]));
+      for (var i = 0; i < products.length; i++) {
+        postsToAdd.push(createNewRow(products[i]));
       }
       blogContainer.append(postsToAdd);
     }
   
-    // This function constructs a post's HTML
-    function createNewRow(post) {
-      var formattedDate = new Date(post.createdAt);
+    // This function constructs a product's HTML
+    function createNewRow(product) {
+      var formattedDate = new Date(product.createdAt);
       formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
       var newPostPanel = $("<div>");
       newPostPanel.addClass("panel panel-default");
@@ -80,7 +80,7 @@ $(document).ready(function() {
       var newPostTitle = $("<h2>");
       var newPostDate = $("<small>");
       var newPostAuthor = $("<h5>");
-      newPostAuthor.text("Written by: Author name display is in next activity when we learn joins!");
+      newPostAuthor.text("Written by: " + product.Grower.name);
       newPostAuthor.css({
         float: "right",
         color: "blue",
@@ -90,8 +90,8 @@ $(document).ready(function() {
       var newPostPanelBody = $("<div>");
       newPostPanelBody.addClass("panel-body");
       var newPostBody = $("<p>");
-      newPostTitle.text(post.title + " ");
-      newPostBody.text(post.body);
+      newPostTitle.text(product.title + " ");
+      newPostBody.text(product.body);
       newPostDate.text(formattedDate);
       newPostTitle.append(newPostDate);
       newPostPanelHeading.append(deleteBtn);
@@ -101,39 +101,39 @@ $(document).ready(function() {
       newPostPanelBody.append(newPostBody);
       newPostPanel.append(newPostPanelHeading);
       newPostPanel.append(newPostPanelBody);
-      newPostPanel.data("post", post);
+      newPostPanel.data("product", product);
       return newPostPanel;
     }
-  
-    // This function figures out which post we want to delete and then calls deletePost
+    
+    // This function figures out which product we want to delete and then calls deletePost
     function handlePostDelete() {
       var currentPost = $(this)
         .parent()
         .parent()
-        .data("post");
+        .data("product");
       deletePost(currentPost.id);
     }
   
-    // This function figures out which post we want to edit and takes it to the appropriate url
+    // This function figures out which product we want to edit and takes it to the appropriate url
     function handlePostEdit() {
       var currentPost = $(this)
         .parent()
         .parent()
-        .data("post");
+        .data("product");
       window.location.href = "/cms?post_id=" + currentPost.id;
     }
   
-    // This function displays a messgae when there are no posts
+    // This function displays a messgae when there are no products
     function displayEmpty(id) {
       var query = window.location.search;
       var partial = "";
       if (id) {
-        partial = " for Author #" + id;
+        partial = " for Grower #" + id;
       }
       blogContainer.empty();
       var messageh2 = $("<h2>");
       messageh2.css({ "text-align": "center", "margin-top": "50px" });
-      messageh2.html("No posts yet" + partial + ", navigate <a href='/cms" + query +
+      messageh2.html("No products yet" + partial + ", navigate <a href='/cms" + query +
       "'>here</a> in order to get started.");
       blogContainer.append(messageh2);
     }
